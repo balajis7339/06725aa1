80076d03b40cc8cd43165ba5c508d0a127e164af
balajis7339 <70472667+balajis7339@users.noreply.github.com>
2020-12-02 06:32:35 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,16 @@
 <?php // Do not put any HTML above this line
+
 if ( isset($_POST['cancel'] ) ) {
     // Redirect the browser to game.php
     header("Location: index.php");
     return;
 }
+
 $salt = 'XyZzy12*_';
-$stored_hash = '1a52e17fa899cf40fb04cfc42e6352f1';  // Pw is meow123
+$stored_hash = 'a8609e8d62c043243c4e201cbb342862';  // Pw is meow123
+
 $failure = false;  // If we have no POST data
+
 // Check to see if we have some POST data, if we do process it
 if ( isset($_POST['who']) && isset($_POST['pass']) ) {
     if ( strlen($_POST['who']) < 1 || strlen($_POST['pass']) < 1 ) {
@@ -22,13 +26,14 @@ if ( isset($_POST['who']) && isset($_POST['pass']) ) {
         }
     }
 }
+
 // Fall through into the View
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <?php require_once "bootstrap.php"; ?>
-<title>Anamitra Musib 32ce3337</title>
+<title>Chuck Severance's Login Page</title>
 </head>
 <body>
 <div class="container">
@@ -52,7 +57,8 @@ if ( $failure !== false ) {
 <p>
 For a password hint, view source and find a password hint
 in the HTML comments.
-<!-- Hint: The password is the 3  character language we are learning (all lower case) followed by 123. -->
+<!-- Hint: The password is the four character sound a cat
+makes (all lower case) followed by 123. -->
 </p>
 </div>
 </body>
